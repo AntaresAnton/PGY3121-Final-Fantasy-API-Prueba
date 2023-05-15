a47b1290f260672970d39c28fc70a1b05fc6a746
Patricio Quintanilla <86749917+AntaresAnton@users.noreply.github.com>
2023-05-14 23:30:45 -0400

    modal

--- js/API.js
+++ js/API.js
@@ -36,7 +36,29 @@ const apiFinalFantasy=async(pagina)=>{
                     <li class="list-group-item"><b>Raza: </b>${character.race}</li>                    
                   </ul>
                   <div class="card-body text-center">
-                    <a class="btn btn-primary" href="#">Más información de ${character.name}</a>
+                  <!-- Button trigger modal -->
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    Más información de ${character.name}
+                  </button>
+                  
+                  <!-- Modal -->
+                  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="staticBackdropLabel">Información de ${character.name}</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          <p>${character.description}</p>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                   </div>
                 </div>
                 <!-- fin card -->
