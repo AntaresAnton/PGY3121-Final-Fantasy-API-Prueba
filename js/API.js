13955797b1396f7a1fca958a3ebd2fe89e2ba21a
Patricio Quintanilla <86749917+AntaresAnton@users.noreply.github.com>
2023-05-14 23:21:34 -0400

    detalles

--- js/API.js
+++ js/API.js
@@ -18,28 +18,29 @@ const apiFinalFantasy=async(pagina)=>{
         divItem=document.createElement('div')
         // El card
             divItem.innerHTML=`
-            <div class="col-md-3 g-0">
-  <div class="card mx-2" style="width: 18rem;">
-    <img src="${imagenes}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${character.name}</h5>
-      <p class="card-text"></p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item"><b>Nombre (JP): </b>${character.japaneseName}</li>
-      <li class="list-group-item"><b>Job: </b>${character.job}</li>
-      <li class="list-group-item"><b>Origen: </b>${character.origin}</li>
-      <li class="list-group-item"><b>Especie: </b>${character.species}</li>
-      <li class="list-group-item"><b>Edad: </b>${character.age}</li>
-      <li class="list-group-item"><b>Género: </b>${character.gender}</li>
-      <li class="list-group-item"><b>Raza: </b>${character.race}</li>                    
-    </ul>
-    <div class="card-body text-center">
-      <a class="btn btn-primary" href="#">Más información de ${character.name}</a>
-    </div>
-  </div>
-  <!-- fin card -->
-</div>
+            <!-- Inicio Card -->
+            <div class="col-md-3 col-sm-6 col-xs-12 g-0">
+                <div class="card mx-2" style="width: 18rem;">
+                  <img src="${imagenes}" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">${character.name}</h5>
+                    <p class="card-text"></p>
+                  </div>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><b>Nombre (JP): </b>${character.japaneseName}</li>
+                    <li class="list-group-item"><b>Job: </b>${character.job}</li>
+                    <li class="list-group-item"><b>Origen: </b>${character.origin}</li>
+                    <li class="list-group-item"><b>Especie: </b>${character.species}</li>
+                    <li class="list-group-item"><b>Edad: </b>${character.age}</li>
+                    <li class="list-group-item"><b>Género: </b>${character.gender}</li>
+                    <li class="list-group-item"><b>Raza: </b>${character.race}</li>                    
+                  </ul>
+                  <div class="card-body text-center">
+                    <a class="btn btn-primary" href="#">Más información de ${character.name}</a>
+                  </div>
+                </div>
+                <!-- fin card -->
+              </div>
             `
             divRes.appendChild(divItem);
     });
