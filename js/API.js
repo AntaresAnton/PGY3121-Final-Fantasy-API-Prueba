ccb7312bd6a418907794ab74460b4825db94a90a
Patricio Quintanilla <86749917+AntaresAnton@users.noreply.github.com>
2023-05-14 23:49:24 -0400

    Update API.js

--- js/API.js
+++ js/API.js
@@ -19,109 +19,57 @@ const apiFinalFantasy=async(pagina)=>{
         // El card
             divItem.innerHTML=`
             <!-- Inicio Card -->
-<div class="col-md-3 col-sm-6 col-xs-12 g-0">
-  <div class="card mx-2" style="width: 18rem;">
-    <img src="${imagenes}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${character.name}</h5>
-      <p class="card-text"></p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item"><b>Nombre (JP): </b>${character.japaneseName}</li>
-      <li class="list-group-item"><b>Job: </b>${character.job}</li>
-      <li class="list-group-item"><b>Origen: </b>${character.origin}</li>
-      <li class="list-group-item"><b>Especie: </b>${character.species}</li>
-      <li class="list-group-item"><b>Edad: </b>${character.age}</li>
-      <li class="list-group-item"><b>Género: </b>${character.gender}</li>
-      <li class="list-group-item"><b>Raza: </b>${character.race}</li>                    
-    </ul>
-    <div class="card-body text-center">
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-      Más información de ${character.name}
-    </button>
-    
-    <!-- Modal -->
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">Información de ${character.name}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-          <img src="${imagenes}" class="card-img-top w-50" alt="...">
-          <li class="list-group-item"><b>Nombre (JP): </b>${character.japaneseName}</li>
-          <li class="list-group-item"><b>Job: </b>${character.job}</li>
-          <li class="list-group-item"><b>Origen: </b>${character.origin}</li>
-          <li class="list-group-item"><b>Especie: </b>${character.species}</li>
-          <li class="list-group-item"><b>Edad: </b>${character.age}</li>
-          <li class="list-group-item"><b>Género: </b>${character.gender}</li>
-          <li class="list-group-item"><b>Raza: </b>${character.race}</li>   
-            <p>${character.description}</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Inicio Card -->
-    <div class="col-md-3 col-sm-6 col-xs-12 g-0">
-      <div class="card mx-2" style="width: 18rem;">
-        <img src="${imagenes}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${character.name}</h5>
-          <p class="card-text"></p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item"><b>Nombre (JP): </b>${character.japaneseName}</li>
-          <li class="list-group-item"><b>Job: </b>${character.job}</li>
-          <li class="list-group-item"><b>Origen: </b>${character.origin}</li>
-          <li class="list-group-item"><b>Especie: </b>${character.species}</li>
-          <li class="list-group-item"><b>Edad: </b>${character.age}</li>
-          <li class="list-group-item"><b>Género: </b>${character.gender}</li>
-          <li class="list-group-item"><b>Raza: </b>${character.race}</li>                    
-        </ul>
-        <div class="card-body text-center">
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-          Más información de ${character.name}
-        </button>
-        
-        <!-- Modal -->
-        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Información de ${character.name}</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="col-md-3 col-sm-6 col-xs-12 g-0">
+                <div class="card mx-2" style="width: 18rem;">
+                  <img src="${imagenes}" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">${character.name}</h5>
+                    <p class="card-text"></p>
+                  </div>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><b>Nombre (JP): </b>${character.japaneseName}</li>
+                    <li class="list-group-item"><b>Job: </b>${character.job}</li>
+                    <li class="list-group-item"><b>Origen: </b>${character.origin}</li>
+                    <li class="list-group-item"><b>Especie: </b>${character.species}</li>
+                    <li class="list-group-item"><b>Edad: </b>${character.age}</li>
+                    <li class="list-group-item"><b>Género: </b>${character.gender}</li>
+                    <li class="list-group-item"><b>Raza: </b>${character.race}</li>                    
+                  </ul>
+                  <div class="card-body text-center">
+                  <!-- Button trigger modal -->
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    Más información de ${character.name}
+                  </button>
+                  
+                  <!-- Modal -->
+                  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="staticBackdropLabel">Información de ${character.name}</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                        <img src="${imagenes}" class="card-img-top w-50" alt="...">
+                        <li class="list-group-item"><b>Nombre (JP): </b>${character.japaneseName}</li>
+                        <li class="list-group-item"><b>Job: </b>${character.job}</li>
+                        <li class="list-group-item"><b>Origen: </b>${character.origin}</li>
+                        <li class="list-group-item"><b>Especie: </b>${character.species}</li>
+                        <li class="list-group-item"><b>Edad: </b>${character.age}</li>
+                        <li class="list-group-item"><b>Género: </b>${character.gender}</li>
+                        <li class="list-group-item"><b>Raza: </b>${character.race}</li>   
+                          <p>${character.description}</p>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+                <!-- fin card -->
               </div>
-              <div class="modal-body">
-              <img src="${imagenes}" class="card-img-top w-50" alt="...">
-              <li class="list-group-item"><b>Nombre (JP): </b>${character.japaneseName}</li>
-              <li class="list-group-item"><b>Job: </b>${character.job}</li>
-              <li class="list-group-item"><b>Origen: </b>${character.origin}</li>
-              <li class="list-group-item"><b>Especie: </b>${character.species}</li>
-              <li class="list-group-item"><b>Edad: </b>${character.age}</li>
-              <li class="list-group-item"><b>Género: </b>${character.gender}</li>
-              <li class="list-group-item"><b>Raza: </b>${character.race}</li>   
-                <p>${character.description}</p>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        </div>
-      </div>
-      <!-- fin modal -->
-    </div>
-    </div>
-  </div>
-  <!-- fin card -->
-</div>
             `
             divRes.appendChild(divItem);
     });
